724606244d62c86f6d2ce288e844847aeeaeee13
mathe <matheusasturofc@gmail.com>
2026-09-25 14:50:34 -0300

    Menu lateral do painel mais parecido com o do Aternos: coluna escura, servidor no topo, itens grandes e botao de recolher

--- js/i18n-en.js
+++ js/i18n-en.js
@@ -696,6 +696,7 @@ const I18N_EXACT = {
 
   // ---- painel: opções (configurações do jogo e gamerules)
   "Geral": "General",
+  "Recolher o menu": "Collapse menu",
   "Configuração": "Settings",
   "Dados": "Data",
   "Configurações do jogo": "Game settings",
